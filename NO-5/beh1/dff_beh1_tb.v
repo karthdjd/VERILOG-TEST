b4cefd591c1ec819eabcb54bca73b022c3efe212
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-02-25 12:05:36 +0530

    Create dff_beh1_tb.v

---
+++ NO-5/beh1/dff_beh1_tb.v
@@ -0,0 +1,18 @@
+module dff_beh1_tb;  
+    reg clock;  
+    reg d;  
+   output q;
+ dff_beh1  dut( .d(d),   
+                .clock (clock),  
+                .q (q));  
+  always #10 clock = clock;  
+ initial begin  
+        clock = 1;  
+        d = 1;  
+       #15 d = 0;  
+       #30 d=1;
+       #45 d=0;
+        #100 $finish;
+	$display ("%0t, d=%0b q=%0b", $time, d, q);
+    end  
+endmodule  
