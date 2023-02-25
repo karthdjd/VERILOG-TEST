28230b531f32cec04a84d6806cd0bf2f34805b91
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-02-25 11:57:43 +0530

    Update 5_beh_tb.v

--- NO-5/beh/5_beh_tb.v
+++ NO-5/beh/5_beh_tb.v
@@ -1,25 +1,15 @@
-module dff_tb;  
+module dff_beh_tb;  
     reg clock;  
     reg d;  
-      
-    
-  output q;
- 
-  
-    dff_beh  dut( .d(d),   
+   output q;
+ dff_beh  dut( .d(d),   
                 .clock (clock),  
                 .q (q));  
-  
-   
-    always #10 clock = clock;  
- 
-   
-    initial begin  
+  always #10 clock = clock;  
+ initial begin  
         clock = 1;  
         d = 1;  
-         
-  
-        #15 d = 0;  
+       #15 d = 0;  
        #30 d=1;
        #45 d=0;
         #100 $finish;
